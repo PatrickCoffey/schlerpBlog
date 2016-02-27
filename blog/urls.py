973e6acc002dc0d9cef9e047aa68c243d19b8d11
schlerp <patrickcoffey91@gmail.com>
2016-02-28 01:50:59 +0930

    added pagination

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,17 @@ from django.conf.urls import patterns, url
 from django.views.generic import ListView
 from blog.models import Post
 
+pag_num = 3
+
 urlpatterns = patterns('',
     # Index
-    url('^$', ListView.as_view(model=Post, 
-                               template_name='templates/post_list.html',
-                               )),
+    #url('^$', ListView.as_view(model=Post, 
+                               #template_name='templates/post_list.html',
+                               #paginate_by=pag_num,
+                               #)),
+    
+    url(r'^(?P<page>\d+)?/?$', ListView.as_view(model=Post,
+                                                template_name='templates/post_list.html',
+                                                paginate_by=pag_num), name='index'),    
+    
 )
--- schlerpBlog/urls.py
+++ schlerpBlog/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     #url(r'^blog/', include('blog.urls')),
-    url(r'^.*$', include('blog.urls')),
+    url(r'', include('blog.urls')),
+    
 )
